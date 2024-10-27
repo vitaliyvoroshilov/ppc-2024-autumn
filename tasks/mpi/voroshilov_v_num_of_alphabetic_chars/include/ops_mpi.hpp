b4e5055e8c28a0214d6de1ec1b6cca1b6749eb27
vitaliivoroshilov <vitallizer@yandex.ru>
2024-10-28 00:43:43 +0300

    Fix3 in order to pass clang-format

--- tasks/mpi/voroshilov_v_num_of_alphabetic_chars/include/ops_mpi.hpp
+++ tasks/mpi/voroshilov_v_num_of_alphabetic_chars/include/ops_mpi.hpp
@@ -15,30 +15,30 @@
 namespace voroshilov_v_num_of_alphabetic_chars_mpi {
 
 class AlphabetCharsTaskSequential : public ppc::core::Task {
-  public:
-    explicit AlphabetCharsTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-    bool validation() override;
-    bool pre_processing() override;
-    bool run() override;
-    bool post_processing() override;
+ public:
+  explicit AlphabetCharsTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool validation() override;
+  bool pre_processing() override;
+  bool run() override;
+  bool post_processing() override;
 
-  private:
-    std::vector<char> input_;
-    int res_;
+ private:
+  std::vector<char> input_;
+  int res_;
 };
 
 class AlphabetCharsTaskParallel : public ppc::core::Task {
-  public:
-    explicit AlphabetCharsTaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-    bool validation() override;
-    bool pre_processing() override;
-    bool run() override;
-    bool post_processing() override;
+ public:
+  explicit AlphabetCharsTaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool validation() override;
+  bool pre_processing() override;
+  bool run() override;
+  bool post_processing() override;
 
-  private:
-    std::vector<char> input_, local_input_;
-    int res_;
-    boost::mpi::communicator world;
+ private:
+  std::vector<char> input_, local_input_;
+  int res_;
+  boost::mpi::communicator world;
 };
 
 }  // namespace voroshilov_v_num_of_alphabetic_chars_mpi
--- tasks/seq/voroshilov_v_num_of_alphabetic_chars/include/ops_seq.hpp
+++ tasks/seq/voroshilov_v_num_of_alphabetic_chars/include/ops_seq.hpp
@@ -8,16 +8,16 @@
 namespace voroshilov_v_num_of_alphabetic_chars_seq {
 
 class AlphabetCharsTaskSequential : public ppc::core::Task {
-  public:
-    explicit AlphabetCharsTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-    bool validation() override;
-    bool pre_processing() override;
-    bool run() override;
-    bool post_processing() override;
+ public:
+  explicit AlphabetCharsTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool validation() override;
+  bool pre_processing() override;
+  bool run() override;
+  bool post_processing() override;
 
-  private:
-    std::vector<char> input_;
-    int res_;
+ private:
+  std::vector<char> input_;
+  int res_;
 };
 
 }  // namespace voroshilov_v_num_of_alphabetic_chars_seq
